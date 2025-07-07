b510140f505456bdc9d5cc403abb147f5ac20a28
sunithagithub20 <lakshmisunitha20@gmail.com>
2025-07-07 21:38:44 +0530

    Initial commit - ApexPlanet Task 5 Portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+    //     reset: true
+});
+
+sr.reveal('.home__data, .home__img, .home__social', {});
+sr.reveal('.about__img, .about__subtitle, .about__text', {
+    delay: 400
+});
+sr.reveal('.skills-container, .skill-card', {
+    interval: 200
+}); // 👉 ADD THIS
+sr.reveal('.work__img', {
+    interval: 200
+});
+sr.reveal('.contact__input', {
+    interval: 200
+});
+window.addEventListener("DOMContentLoaded", () => {
+    if (window.innerWidth <= 768) {
+        const blobGroup = document.querySelector(".home__blob-group");
+        if (blobGroup) {
+            blobGroup.setAttribute("transform", "scale(15) translate(, 50)");
+        }
+    }
+});
